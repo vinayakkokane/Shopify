f68a94f6eaf7a420079af39be1d7f576cec92135
vinayak kokane <vinayaklkokane2001@gmail.com>
2021-04-07 09:03:41 +0530

    My first commit

---
+++ src/data/product_data.js
@@ -0,0 +1,51 @@
+const product_card = [
+    {
+        id: 1,
+        product_name: "Baggit Bags",
+        description: "Utility bag, Full leather at best price",
+        price: 1100,
+        currency: "Rs",
+        thumb: "./images/1.png"
+    },
+    {
+        id: 2,
+        product_name: "Sapiens Book",
+        description: "A brief history of human-mankind, bestsller!",
+        price: 400,
+        currency: "Rs",
+        thumb: "./images/2.png"
+    },
+    {
+        id: 3,
+        product_name: "BoAt Headphone",
+        description: "BoAt Rockerz 450, premium build-up, heavy bass!",
+        price: 1450,
+        currency: "Rs",
+        thumb: "./images/3.png"
+    },
+    {
+        id: 4,
+        product_name: "Oppo Mobile",
+        description: "Oppo A5, (64G,4GB) Mirror Blue/Black",
+        price: 12000,
+        currency: "Rs",
+        thumb: "./images/4.png"
+    },
+    {
+        id: 5,
+        product_name: "Adidas Shoe",
+        description: "Men's Running shoe at best price",
+        price: 3000,
+        currency: "Rs",
+        thumb: "./images/5.png"
+    },
+    {
+        id: 6,
+        product_name: "JBL Headphone",
+        description: "JBL E55BT | Over-ear wireless headphones",
+        price: 2500,
+        currency: "Rs",
+        thumb: "./images/6.png"
+    }
+]
+export default product_card;
